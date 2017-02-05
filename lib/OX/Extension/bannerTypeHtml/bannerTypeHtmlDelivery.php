a81b47ebde7dc975dc84827be50edc18320764f3
moshe <m@inn.co.il>
2017-02-05 16:44:20 +0200

    BANNERTYPE

--- lib/OX/Extension/bannerTypeHtml/bannerTypeHtmlDelivery.php
+++ lib/OX/Extension/bannerTypeHtml/bannerTypeHtmlDelivery.php
@@ -28,7 +28,8 @@
  *
  * @return string               The HTML to display this ad
  */
-function Plugin_BannerTypeHTML_delivery_adRender(&$aBanner, $zoneId=0, $source='', $ct0='', $withText=false, $logClick=true, $logView=true, $useAlt=false, $richMedia=true, $loc, $referer)
+if (!function_exists("Plugin_BannerTypeHTML_delivery_adRender"))  {
+ function Plugin_BannerTypeHTML_delivery_adRender(&$aBanner, $zoneId=0, $source='', $ct0='', $withText=false, $logClick=true, $logView=true, $useAlt=false, $richMedia=true, $loc, $referer)
 {
     $conf = $GLOBALS['_MAX']['CONF'];
     $prepend = !empty($aBanner['prepend']) ? $aBanner['prepend'] : '';
@@ -73,5 +74,5 @@ function Plugin_BannerTypeHTML_delivery_adRender(&$aBanner, $zoneId=0, $source='
     }
     return $prepend . $code . $bannerText . $beaconTag . $append;
 }
-
+}
 ?>
